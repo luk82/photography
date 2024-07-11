58253ff6b8cc63a226c5773eb09bf8b9d80f8ecf
LC <lcweb@proton.me>
2024-07-11 16:20:15 +0100

    Update svelte config

---
+++ build/_app/immutable/entry/start.CublblPk.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CwWfBbk6.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,13 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
